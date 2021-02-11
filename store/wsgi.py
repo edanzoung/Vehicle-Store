553925d909c317b95e18606e9ff9cea44893883d
elkanazoungrana <57450098+elkanazoungrana@users.noreply.github.com>
2021-02-11 10:53:28 +0000

    Update wsgi.py

--- store/wsgi.py
+++ store/wsgi.py
@@ -7,8 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 import os
-from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "store.settings")
+from django.core.wsgi import get_wsgi_application
 
-application = DjangoWhiteNoise()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
+
+application = get_wsgi_application()
