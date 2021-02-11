34819f7608f71bca7a074926f18edcec42cca9bb
elkanazoungrana <57450098+elkanazoungrana@users.noreply.github.com>
2021-02-11 14:19:28 +0000

    Update settings.py

--- store/settings.py
+++ store/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATIC_ROOT= os.path.join(BASE_DIR, "staticfiles")
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-STATIC_URL = "../static/"
-STATICFILES_DIRS= [os.path.join(BASE_DIR, "static")]
+STATIC_URL = "/static/"
+#STATICFILES_DIRS= [os.path.join(BASE_DIR, "static")]
 
 
 
